f3c2a8ba9d84154e2e83ed5d892042a19d320129
yukimasa ikeda <ykms123456789@gmail.com>
2019-08-14 17:27:03 +0900

    Finish static pages

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,24 +1,33 @@
 require 'rails_helper'
 
 Rails.describe 'StaticPages', type: :system do
+  let (:basetitle) {  "Ruby on Rails Tutorial Sample App" }
+
   describe 'Home' do
-    it '画面の表示' do
-      visit 'static_pages/home'
-      expect(page).to have_css('h1', text: 'Sample App')
+    it 'タイトル内容の表示' do
+      visit '/'
+      expect(page).to have_title "Home | #{basetitle}"
     end
   end
 
   describe 'Help' do
-    it '画面の表示' do
+    it 'タイトル内容の表示' do
       visit 'static_pages/help'
-      expect(page).to have_css('h1', text: 'Help')
+      expect(page).to have_title "Help | #{basetitle}"
     end
   end
 
   describe 'About' do
-    it '画面の表示' do
+    it 'タイトル内容の表示' do
       visit 'static_pages/about'
-      expect(page).to have_css('h1', text: 'About')
+      expect(page).to have_title "About | #{basetitle}"
+    end
+  end
+
+  describe 'About' do
+    it 'タイトル内容の表示' do
+      visit 'static_pages/contact'
+      expect(page).to have_title "Contact | #{basetitle}"
     end
   end
 end
