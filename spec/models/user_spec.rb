a0340327f6848fef23573957f70753360c44f833
yukimasa ikeda <ykms123456789@gmail.com>
2019-08-20 21:46:41 +0900

    Complete up to 6.2.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,35 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.create(:user) }
 
-  it "should be valid" do
+  it "ユーザーは有効であるか" do
     expect(user).to be_valid
   end
 
-  it "name should be present" do
+  it "ユーザーは存在するか" do
+    user.name = ""
+    expect(user).to_not be_valid
+  end
+
+  it "メールは存在するか" do
+    user.email = ""
+    expect(user).to_not be_valid
+  end
+
+  it "名前は50文字以下か" do
+    user.name = "a" * 51
+    expect(user).to_not be_valid
+  end
+
+  it "メールは252文字以下か" do
+    user.email = "a" * 244 + "@example.com"
+    expect(user).to_not be_valid
+  end
+
+  it "有効なメールフォーマットか" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).to be_valid, "#{valid_address.inspect} should be valid"
+    end
   end
 end
